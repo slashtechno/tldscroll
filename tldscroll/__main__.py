ffeb63038e919a3750b27c18eae245a224d58637
slashtechno <77907286+slashtechno@users.noreply.github.com>
2024-08-21 18:31:27 -0400

    feat: Send a normal message and an ephemeral message

--- tldscroll/__main__.py
+++ tldscroll/__main__.py
@@ -2,18 +2,43 @@ import os
 import dotenv
 # Slack imports
 from slack_bolt import App
-from slack_bolt.adapter.socket_mode import SocketModeHandler 
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_sdk.web.client import WebClient
+
+# app = None
+dotenv.load_dotenv()
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
+
+if (not SLACK_BOT_TOKEN or not SLACK_APP_TOKEN) or not (
+    SLACK_BOT_TOKEN.startswith("xoxb-") and SLACK_APP_TOKEN.startswith("xapp-")
+):  
+    # FATA, ERRO, DEBU, or WARN for equal spacing
+    print("FATA: Invalid Slack tokens")
+    exit(1)
+
+
+app = App(token=SLACK_BOT_TOKEN)
 
 
 def main():
-    dotenv.load_dotenv()
-    SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
-    SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
-
-    if (not SLACK_BOT_TOKEN or not SLACK_APP_TOKEN) or not (SLACK_BOT_TOKEN.startswith("xoxb-") and SLACK_APP_TOKEN .startswith("xapp-")):
-        # FATA, ERRO, DEBU, or WARN for equal spacing
-        print("FATA: Invalid Slack tokens")
-
-
-    app = App(token=SLACK_BOT_TOKEN)
+    global app
     SocketModeHandler(app, SLACK_APP_TOKEN).start()
+
+
+@app.message(":wave:")
+# https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
+def message_hello(message, say, client: WebClient):
+    # say() sends a message to the channel where the event was triggered
+    say(f"Hey there <@{message['user']}>!")
+    # This will only send a message to the user who triggered the event
+    client.chat_postEphemeral(
+        text="Hey there!",
+        channel=message["channel"],
+        user=message["user"],
+    )
+
+
+
+if __name__ == "__main__":
+    main()
