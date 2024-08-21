586483d996671bd84943e4661d3cdea081ca31e6
slashtechno <77907286+slashtechno@users.noreply.github.com>
2024-08-21 19:44:42 -0400

    work on LLM integration

---
+++ tldscroll/llm/llm.py
@@ -0,0 +1,28 @@
+from langchain_community.llms import Ollama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+class Summarizer:
+    def __init__(self, model_name: str = "llama3.1"):
+        # self.model = Ollama(model_name=model_name)
+        self._llm = Ollama(model_name=model_name)
+
+    def summarize(self, messages: list) -> str:
+        # https://python.langchain.com/v0.1/docs/modules/model_io/prompts/quick_start/#lcel
+
+        context_messages = [
+            (
+                "system",
+                "Summarize the messages in this conversation. Only output the summary.",
+            ),
+        ]
+        for m in messages:
+            context_messages.append(("user", f"<@{m['user']}>: {m['text']}"))
+
+        prompt_template = ChatPromptTemplate.from_messages(context_messages)
+    
+
+        chain = prompt_template | self._llm | StrOutputParser()
+
+        return chain.invoke()
