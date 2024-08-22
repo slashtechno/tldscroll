fce3d6e1a44add928b15127d7df8c328890bf14e
slashtechno <77907286+slashtechno@users.noreply.github.com>
2024-08-22 16:37:46 -0500

    feat: Add /summarize command to summarize permalinks

--- tldscroll/__main__.py
+++ tldscroll/__main__.py
@@ -1,17 +1,19 @@
 import os
 import dotenv
+import re
 
 # Slack imports
 from slack_bolt import App, BoltResponse
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_sdk.web.client import WebClient
 
 # To avoid a log message about unhandled requests
 from slack_bolt.error import BoltUnhandledRequestError
 
-# Type hinting
-from slack_sdk.web.client import WebClient
+# Types
 from slack_bolt.context.say import Say
-
+from slack_bolt.context.respond import Respond
+from slack_sdk.errors import SlackApiError
 from tldscroll.utils import Summarizer
 
 
@@ -53,12 +55,40 @@ def handle_shortcut(ack, client: WebClient, shortcut: dict, say: Say):
         client=client,
     )
 
+
+@app.command("/summarize")
+def summarize_command(ack, client: WebClient, command: dict, respond: Respond):
+    ack()
+    channel_id = command["channel_id"],
+
+    # Parse permalink
+    # https://api.slack.com/methods/chat.getPermalink#examples
+    # https://api.slack.com/methods/conversations.history#single-message
+    match = re.search(r'/archives/(\w+)/p(\d+)', command["text"])
+    if not match:
+        respond("Invalid permalink")
+        return
+    channel_id = match.group(1)
+    message_ts = f"{match.group(2)[:10]}.{match.group(2)[10:]}"
+
+    summarizer.on_request(
+        channel_id=channel_id,
+        message_ts=message_ts,
+        say=respond,
+        client=client,
+    )
+
 # https://github.com/slackapi/bolt-python/issues/299#issuecomment-823590042    
 @app.error
-def handle_errors(error):
+def handle_errors(error, body, respond):
     if isinstance(error, BoltUnhandledRequestError):
         return BoltResponse(status=200, body="")
     else:
+        print(f"Error: {error.response.data["error"]}")
+        try:
+            respond("Something went wrong. If this persists, please contact <@U075RTSLDQ8>.")
+        except SlackApiError as e:
+            print(f"Error sending message: {e.response['error']}")
         return BoltResponse(status=500, body="Something Wrong")
 
 def main():
--- tldscroll/utils/summarizer.py
+++ tldscroll/utils/summarizer.py
@@ -12,17 +12,21 @@ class Summarizer:
         # self.model = Ollama(model_name=model_name)
         self._llm = ChatOllama(model=model_name)
 
-    def summarize(self, messages: list, bot_username: str) -> str:
+    def summarize(self, messages: list, bot_user_id: str) -> str:
+        """
+        Summarize a conversation thread.
+        """
+
         # https://python.langchain.com/v0.1/docs/modules/model_io/prompts/quick_start/#lcel
         # Create a message history for the conversation
         context_messages = [
             (
                 "system",
-                "Summarize the messages in this conversation. Only output the summary. To mention a user, use <@user_id>. If there's only one message, just summarize it.",
+                "Summarize the messages in this conversation. Only output the summary. To mention a user, use <@user_id>. If there's only one message in the conversation, just summarize that message.",
             ),
         ]
         for m in messages:            
-            if m["user"] == bot_username:  
+            if m["user"] == bot_user_id:  
                 continue
             print(f"{m['user']}: {m['text']}")
             context_messages.append(("user", f"<@{m['user']}>: {m['text']}"))
@@ -35,6 +39,13 @@ class Summarizer:
         return chain.invoke({})
     
     def on_request(self, channel_id: str, message_ts: str, say: Say, client: WebClient):
+        """
+        Summarize a message, or thread, given a channel ID and message timestamp.
+        If the message is in a thread, only the message will be summarized.
+        If the message is a top-level message, the message and all replies will be summarized.
+
+        The bot ignores its own messages when summarizing.
+        """
         replies = client.conversations_replies(
             channel=channel_id,
             ts=message_ts,
@@ -47,8 +58,8 @@ class Summarizer:
         
         
         # Get the bot username so it can be ignored in the summary
-        bot_username = client.auth_test()["user"]
+        bot_user_id = client.auth_test().data["user_id"]
 
-        summary = self.summarize(messages=replies.data["messages"], bot_username=bot_username)
+        summary = self.summarize(messages=replies.data["messages"], bot_user_id=bot_user_id)
         print(f"Summary: {summary}")
         say(summary, thread_ts=message_ts)
